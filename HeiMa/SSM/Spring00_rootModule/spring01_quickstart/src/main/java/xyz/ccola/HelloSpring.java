45aa44945b9295e7c2571d191538b24e1c8436e5
Cola <q1203952894@gmail.com>
2022-12-19 15:09:31 +0800

    初始化项目

---
+++ HeiMa/SSM/Spring00_rootModule/spring01_quickstart/src/main/java/xyz/ccola/HelloSpring.java
@@ -0,0 +1,13 @@
+package xyz.ccola;
+
+/**
+ * @ Name: HelloSpring
+ * @ Author: Cola
+ * @ Time: 2022/12/19 15:08
+ * @ Description: HelloSpring
+ */
+public class HelloSpring {
+    public static void main(String[] args) {
+        System.out.println("Hello Spring");
+    }
+}
